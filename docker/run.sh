543c725b54637eedbbb77b6e423ee11815bf8d5a
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-10-03 20:21:55 +0900

    mod run command

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,13 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+if [ -e $SCRIPT_DIR/../../dataset ]; then
+    DATASET_MOUNT_COMMAND="-v $SCRIPT_DIR/../../dataset:/dataset"
+else
+    DATASET_MOUNT_COMMAND=""
+fi
+
+set -x
+
 docker run -it --rm \
 -u `id -u`:`id -g` \
 --gpus all \
@@ -11,6 +19,6 @@ docker run -it --rm \
 -v /etc/group:/etc/group:ro \
 -v $SCRIPT_DIR/..:/userdir \
 -v $SCRIPT_DIR/homedir:/home/`whoami`/ \
--v $SCRIPT_DIR/../../dataset:/dataset \
+$DATASET_MOUNT_COMMAND \
 -w /userdir \
 headmap_dnn bash
