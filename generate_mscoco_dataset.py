f29ca8aa54320fdb65cc98766d59f3a2cccaed75
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-10-04 13:44:58 +0900

    add fix label mode

--- generate_mscoco_dataset.py
+++ generate_mscoco_dataset.py
@@ -6,7 +6,9 @@ import numpy as np
 if __name__ == '__main__':
     image_path_roots = ['/dataset/coco/train2017', '/dataset/coco/val2017']
     load_annotation_files = ['/dataset/coco/annotations/person_keypoints_train2017.json', '/dataset/coco/annotations/person_keypoints_val2017.json']
-    
+    image_height = 256
+    image_width = 256
+    target_keypoint_id = [6,7,8,9]
     for ds_type, image_path_root, load_annotation_file in zip (['train', 'val'], image_path_roots, load_annotation_files):
         with open(load_annotation_file) as f:
             coco_dict = json.load(f)
@@ -14,6 +16,7 @@ if __name__ == '__main__':
         cnt = 0
         ds_max = 25000 if ds_type == 'train' else 5000
         # ds_max = 10 if ds_type == 'train' else 10
+        os.makedirs(ds_type, exist_ok=True)
         for i, anno in enumerate(coco_dict["annotations"]):
             if anno['iscrowd'] == 1 or anno['num_keypoints'] == 0:
                 continue
@@ -23,12 +26,7 @@ if __name__ == '__main__':
             image = cv2.imread(image_path)
             keypoint_map = np.zeros(image.shape[0:2], dtype=np.uint8)
             keypoint_data = np.array(anno['keypoints']).reshape((-1,3))
-            if (keypoint_data[[6,7,8,9],2]==2).all():
-                for k_id in [6,7,8,9]:
-                    k_x = keypoint_data[k_id][0]
-                    k_y = keypoint_data[k_id][1] 
-                    keypoint_map[k_y-1:k_y+2, k_x-1:k_x+2] = 255
-            else :
+            if not (keypoint_data[target_keypoint_id,2]==2).all():
                 continue
             bbox = np.array(anno['bbox'])
             xs = int(bbox[0])
@@ -36,9 +34,23 @@ if __name__ == '__main__':
             xe = int(bbox[0]+bbox[2])
             ye = int(bbox[1]+bbox[3])
             crop_image = image[ys:ye, xs:xe]
-            crop_keypoint_map = keypoint_map[ys:ye, xs:xe]
-            crop_image_resize = cv2.resize(crop_image, (256,256))
-            crop_keypoint_map_resize = cv2.resize(crop_keypoint_map, (256,256), cv2.INTER_NEAREST)
+            crop_image_resize = cv2.resize(crop_image, (image_width,image_height))
+
+            if False:
+                for k_id in target_keypoint_id:
+                    k_x = keypoint_data[k_id][0]
+                    k_y = keypoint_data[k_id][1] 
+                    keypoint_map[k_y-1:k_y+2, k_x-1:k_x+2] = 255    
+                crop_keypoint_map = keypoint_map[ys:ye, xs:xe]
+                crop_keypoint_map_resize = cv2.resize(crop_keypoint_map, (image_width,image_height), cv2.INTER_NEAREST)
+            else:
+                crop_keypoint_map_resize = np.zeros((image_height,image_width), dtype=np.uint8)
+                for k_id in target_keypoint_id:
+                    k_x = keypoint_data[k_id][0]
+                    k_y = keypoint_data[k_id][1] 
+                    p_x = int((k_x-xs)/(xe-xs)*image_width)
+                    p_y = int((k_y-ys)/(ye-ys)*image_height)
+                    crop_keypoint_map_resize[p_y-1:p_y+2, p_x-1:p_x+2] = 255
             save_image_file = "{0}/image_{1:08d}.png".format(ds_type, i)
             save_label_file = "{0}/label_{1:08d}.png".format(ds_type, i)
             cv2.imwrite(save_image_file, crop_image_resize)
